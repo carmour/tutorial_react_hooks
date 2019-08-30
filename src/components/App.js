12e1a1dcc9e1088a6179ad2af8f70635d4fe096a
carmour <callum.m.armour@gmail.com>
2019-08-30 12:17:25 -0700

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ResourceList from './ResourceList';
 
-class App extends React.Component {
-  state = {
-    resourceName: 'posts'
-  }
+// class App extends React.Component {
+//   state = {
+//     resourceName: 'posts'
+//   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-        <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-        <ResourceList resourceName={this.state.resourceName}/>
-      </React.Fragment>
-    )
-  }
+//   render() {
+// return (
+//   <React.Fragment>
+//     <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
+//     <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
+//     <ResourceList resourceName={this.state.resourceName}/>
+//   </React.Fragment>
+// )
+//   }
+// }
+
+const App = () => {
+
+
+  return (
+    <React.Fragment>
+      <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
+      <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
+      <ResourceList resourceName={this.state.resourceName} />
+    </React.Fragment>
+  )
 }
+
 export default App;
