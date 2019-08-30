4b6f0a3c8be73c0e527928eb0a7168c1276afb37
carmour <callum.m.armour@gmail.com>
2019-08-30 12:28:56 -0700

    app component refactored into function based

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,6 @@ import ResourceList from './ResourceList';
 //   state = {
 //     resourceName: 'posts'
 //   }
-
 //   render() {
 // return (
 //   <React.Fragment>
@@ -17,15 +16,21 @@ import ResourceList from './ResourceList';
 // )
 //   }
 // }
+// this function refactored below into function based component using hooks
 
 const App = () => {
-
+  const [resourceName, setResourceName] = useState('posts');
+  // resourceName is the current state
+  // setResourceName is a function that changes resourceName
+  // useState is a react function that takes in one param === initialState
+  // why an array? array destructuring
+  // basically we have created an array called useState with two values: resourceName and setResourceName
 
   return (
     <React.Fragment>
-      <button onClick={() => this.setState({ resourceName: 'posts' })}>Posts</button>
-      <button onClick={() => this.setState({ resourceName: 'todos' })}>Todos</button>
-      <ResourceList resourceName={this.state.resourceName} />
+      <button onClick={() => setResourceName('posts')}>Posts</button>
+      <button onClick={() => setResourceName('todos')}>Todos</button>
+      <ResourceList resourceName={resourceName} />
     </React.Fragment>
   )
 }
